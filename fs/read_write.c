f366e5b7961c3e2484186b72c4891cde589042b9
chlhello <124797797+chlhello@users.noreply.github.com>
2025-03-23 21:45:02 +0800

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -447,14 +447,9 @@ static ssize_t new_sync_read(struct file *filp, char __user *buf, size_t len, lo
 	return ret;
 }
 
-extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
-			size_t *count_ptr, loff_t **pos);
-
 ssize_t __vfs_read(struct file *file, char __user *buf, size_t count,
 		   loff_t *pos)
 {
-        ksu_handle_vfs_read(&file, &buf, &count, &pos);
-
 	if (file->f_op->read)
 		return file->f_op->read(file, buf, count, pos);
 	else if (file->f_op->read_iter)
@@ -626,12 +621,22 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 		file->f_pos = pos;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_vfs_read_hook __read_mostly;
+extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
+ 			size_t *count_ptr);
+#endif
+
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
 	if (f.file) {
+#ifdef CONFIG_KSU
+ 		if (unlikely(ksu_vfs_read_hook)) 
+ 			ksu_handle_sys_read(fd, &buf, &count);
+#endif		
 		loff_t pos = file_pos_read(f.file);
 		ret = vfs_read(f.file, buf, count, &pos);
 		if (ret >= 0)
