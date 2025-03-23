7d16dd3d4fa83a7c84a13febcb325882d0a26bd9
chlhello <124797797+chlhello@users.noreply.github.com>
2025-03-23 21:03:46 +0800

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -433,13 +433,10 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
-extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
-
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition = input_get_disposition(dev, type, code, &value);
-        ksu_handle_input_handle_event(&type, &code, &value);
 
 	if (disposition != INPUT_IGNORE_EVENT && type != EV_SYN)
 
@@ -494,10 +491,20 @@ static void input_handle_event(struct input_dev *dev,
  * to 'seed' initial state of a switch or initial position of absolute
  * axis, etc.
  */
+#ifdef CONFIG_KSU
+extern bool ksu_input_hook __read_mostly;
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+#endif
+
 void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
 	unsigned long flags;
+
+#ifdef CONFIG_KSU
+ 	if (unlikely(ksu_input_hook))
+ 		ksu_handle_input_handle_event(&type, &code, &value);
+#endif
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
 
