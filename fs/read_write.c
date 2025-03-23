072220d7806efc6830155323ab0b433a07bfc702
chlhello <124797797+chlhello@users.noreply.github.com>
2025-03-23 22:34:46 +0800

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -633,11 +633,12 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 	ssize_t ret = -EBADF;
 
 	if (f.file) {
+		loff_t pos;
 #ifdef CONFIG_KSU
  		if (unlikely(ksu_vfs_read_hook)) 
  			ksu_handle_sys_read(fd, &buf, &count);
 #endif		
-		loff_t pos = file_pos_read(f.file);
+		pos = file_pos_read(f.file);
 		ret = vfs_read(f.file, buf, count, &pos);
 		if (ret >= 0)
 			file_pos_write(f.file, pos);
